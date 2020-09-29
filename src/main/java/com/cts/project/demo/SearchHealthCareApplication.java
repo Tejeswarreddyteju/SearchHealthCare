9411f7a7278db62a4e6d3a559d88fc5ccb9dc313
Tejeswarreddyteju <adityaadhi96@gmail.com>
2020-09-29 13:53:22 -0400

    FirstCommit

---
+++ src/main/java/com/cts/project/demo/SearchHealthCareApplication.java
@@ -0,0 +1,13 @@
+package com.cts.project.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchHealthCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchHealthCareApplication.class, args);
+	}
+
+}
